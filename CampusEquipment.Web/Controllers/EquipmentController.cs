bce835e8bcaaa2d93846b92f65c27ff5bc2370f0
XunilTamtacles <bbondocmattlinux@gmail.com>
2026-09-12 15:01:28 +0800

    i added in controller and equipmentcontroller

---
+++ CampusEquipment.Web/Controllers/EquipmentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CampusEquipment.Web.Controllers
+{
+    public class EquipmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
